1c5a2af304db40da8b60059020b77cff51c98a69
Astweed <johany.bstmn@eduge.ch>
2025-09-02 10:43:46 +0200

    Information importante dans catalogue.php

--- app/views/catalogue.php
+++ app/views/catalogue.php
@@ -1,1 +1,16 @@
 <?php
+
+// Les boutons ajouter pour chaque produit doivent être comme ça :
+/*
+<button class="add-to-cart"
+        data-id="BQ-20"
+        data-sku="BQ-20"
+        data-name="20 roses"
+        data-price="40"
+        data-img="assets/img/20Roses.png">
+            Ajouter
+</button>
+
+*/
+
+// Comme ça, le lient est créé avec la page commande et c'est js qui gère
