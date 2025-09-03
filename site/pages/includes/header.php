a9e6a687dd8a50dffdf30ede5f0307d6e48d86b8
OSSENI YASMINATH MEGANE <YASMINATH.OSSN@ge-pedago.etat-ge.ch>
2025-09-03 10:28:38 +0200

    index header footer terminer

--- site/pages/includes/header.php
+++ site/pages/includes/header.php
@@ -4,30 +4,19 @@
 
 <header class="site-header">
     <div class="header">
-        <!-- Bouton hamburger -->
-        <button class="hamburger" aria-label="Ouvrir le menu" aria-expanded="false" aria-controls="nav-menu">
-            <span></span><span></span><span></span>
-        </button>
 
-        <p class="titre">DkBloom</p>
-
-        <nav id="nav-menu" class="nav">
-            <button class="close-menu" aria-label="Fermer le menu">&times;</button>
-            <ul class="menu">
-                <li><a href="../index.php">Accueil</a></li>
-                <li><a href="">A propos</a></li>
-                <li><a href="">Catalogue</a></li>
-                <li><a href="">Contact</a></li>
-                <li><a href="../inscription.php">S'inscrire</a></li>
-                <li><a href="../interface_connexion.php">Se connecter</a></li>
-            </ul>
-        </nav>
-
-        <div class="icon">
-            <a href="index.php">
-                <img src="../img/iconInscrire-removebg-preview.png" alt="inscrire" width="80">
-                <img src="../img/iconLoupe-removebg-preview.png" alt="recherche" width="50">
-            </a>
+        <div class="h">
+            <p class="titre">DkBloom</p>
+            <nav>
+                <ul class="menu">
+                    <li><a href="../index.php">Accueil</a></li>
+                    <li><a href="">A propos</a></li>
+                    <li><a href="">Catalogue</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="../inscription.php">S'inscrire</a></li>
+                    <li><a  href="../interface_connexion.php">Se connecter</a></li>
+                </ul>
+            </nav>
         </div>
     </div>
 </header>
@@ -52,5 +41,6 @@
     }
 </script>
 
-
+<!--<a href="../inscription.php">S'inscrire</a>
+                <a href="../interface_connexion.php">Se connecter</a>-->
 
--- site/pages/index.php
+++ site/pages/index.php
@@ -13,32 +13,32 @@
 </script>
 
 <body class="corps">
-
+<?php
+include 'includes/header.php';
+?>
 
 <main>
     <section class="entete_accueil">
         <div class="image_texte">
                <div class="texte">
-                   <h1>Bienvenu <span class="accent">élégance</span></h1>
+                   <h1>Bienvenue <span class="accent">élégance</span></h1>
                    <p class="paragraphe">L’art floral intemporel, au service d’une expérience unique et raffinée. La beauté qui ne fane jamais.</p>
                    <br>
                    <div class="btn_accueil">
                        <a class="btn_index" href="catalogue.php">Découvrir nos créations</a>
-                       <a class="btn_index" href="personnalisation.php">Créer la vôtre</a>
+                       <a class="btn_index" href="interface_selection_produit.php">Créer la vôtre</a>
                    </div>
                </div>
                 <div class="bouquet">
-                    <img class="boxerouge" src="img/boxe_rouge_DK.png" alt="" width="200px" height="auto">
-
+                    <img class="boxerouge" src="img/boxe_rouge_DK.png" alt="" >
                 </div>
-                <br>
-
         </div>
-
     </section>
 
 </main>
-
+<?php
+include 'includes/footer.php';
+?>
 </body>
 </html>
 
