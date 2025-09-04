848c948f7ee8db944231c3073e61aadef2606921
Astweed <johany.bstmn@eduge.ch>
2025-09-04 15:19:57 +0200

    Résolution problème ajout commande

--- site/pages/interface_emballage.php
+++ site/pages/interface_emballage.php
@@ -15,27 +15,27 @@
 <div id="emballage" class="catalogue">
     <div>
         <img src="img/emballage_blanc.PNG" alt="Emballage blanc">
-        <button class="add-to-cart" data-id="1">Ajouter</button>
+        <button class="add-to-cart" data-emb-id="1">Ajouter</button>
     </div>
 
     <div>
         <img src="img/emballage_noir.PNG" alt="Emballage noir">
-        <button class="add-to-cart" data-id="2">Ajouter</button>
+        <button class="add-to-cart" data-emb-id="2">Ajouter</button>
     </div>
 
     <div>
         <img src="img/emballage_rose.PNG" alt="Emballage rose">
-        <button class="add-to-cart" data-id="3">Ajouter</button>
+        <button class="add-to-cart" data-emb-id="3">Ajouter</button>
     </div>
 
     <div>
         <img src="img/emballage_gris.PNG" alt="Emballage gris">
-        <button class="add-to-cart" data-id="4">Ajouter</button>
+        <button class="add-to-cart" data-emb-id="4">Ajouter</button>
     </div>
 
     <div>
         <img src="img/emballage_violet.PNG" alt="Emballage violet">
-        <button class="add-to-cart" data-id="5">Ajouter</button>
+        <button class="add-to-cart" data-emb-id="5">Ajouter</button>
     </div>
 </div>
 
--- site/pages/interface_supplement.php
+++ site/pages/interface_supplement.php
@@ -16,63 +16,63 @@
         <img src="img/ours_blanc.PNG" alt="Mini ourson">
         <h3>Mini ourson</h3>
         <p>2 CHF</p>
-        <button class="add-to-cart" data-id="1">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="1">Ajouter</button>
     </div>
 
     <div>
         <img src="img/happybirthday.PNG" alt="Décoration anniversaire">
         <h3>Décoration anniversaire</h3>
         <p>2 CHF</p>
-        <button class="add-to-cart" data-id="2">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="2">Ajouter</button>
     </div>
 
     <div>
         <img src="img/papillon_doree.PNG" alt="Papillons">
         <h3>Papillons</h3>
         <p>2 CHF</p>
-        <button class="add-to-cart" data-id="3">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="3">Ajouter</button>
     </div>
 
     <div>
         <img src="img/baton_coeur.PNG" alt="Bâton cœur">
         <h3>Bâton cœur</h3>
         <p>2 CHF</p>
-        <button class="add-to-cart" data-id="4">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="4">Ajouter</button>
     </div>
 
     <div>
         <img src="img/diamant.PNG" alt="Diamant">
         <h3>Diamant</h3>
         <p>5 CHF</p>
-        <button class="add-to-cart" data-id="5">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="5">Ajouter</button>
     </div>
 
     <div>
         <img src="img/couronne.PNG" alt="Couronne">
         <h3>Couronne</h3>
         <p>5 CHF</p>
-        <button class="add-to-cart" data-id="6">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="6">Ajouter</button>
     </div>
 
     <div>
         <img src="img/paillette_argent.PNG" alt="Paillettes">
         <h3>Paillettes</h3>
         <p>9 CHF</p>
-        <button class="add-to-cart" data-id="7">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="7">Ajouter</button>
     </div>
 
     <div>
         <img src="img/lettre.JPG" alt="Lettre">
         <h3>Lettre</h3>
         <p>10 CHF</p>
-        <button class="add-to-cart" data-id="8">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="8">Ajouter</button>
     </div>
 
     <div>
         <img src="img/carte.PNG" alt="Carte pour mot">
         <h3>Carte pour mot</h3>
         <p>3 CHF</p>
-        <button class="add-to-cart" data-id="9">Ajouter</button>
+        <button class="add-to-cart" data-sup-id="9">Ajouter</button>
     </div>
 </div>
 
