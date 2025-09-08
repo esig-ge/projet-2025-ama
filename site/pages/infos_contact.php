35da3b2412fab36f64f6241e6d3a30709a07c6ae
OSSENI YASMINATH MEGANE <YASMINATH.OSSN@ge-pedago.etat-ge.ch>
2025-09-08 11:36:30 +0200

    p MAJinfo contactt

--- site/pages/infos_contact.php
+++ site/pages/infos_contact.php
@@ -1,3 +1,4 @@
 <?php ?>
 
-page infos envoyer par le client a l'admin. donc si l'admin se connect il peit voir sa rebrique infos_client.
+<h1>page infos envoyer par le client a l'admin. donc si l'admin se connect il peit voir sa rebrique infos_client.
+</h1>
