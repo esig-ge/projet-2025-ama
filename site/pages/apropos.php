32ad5b8f5b4d2c2deaf1b5aa4ab2baa5c11132b3
OSSENI YASMINATH MEGANE <YASMINATH.OSSN@ge-pedago.etat-ge.ch>
2025-09-12 09:55:29 +0200

    page a propos

--- site/pages/apropos.php
+++ site/pages/apropos.php
@@ -1,8 +1,4 @@
 
-
-<?php include 'includes/header.php'; ?>
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,3 +7,33 @@
     <title>DK Bloom — Catalogue bouquet</title>
     <link rel="stylesheet" href="css/style_connexion_inscription.css">
 </head>
+<body>
+<?php include 'includes/header.php'; ?>
+<main>
+    <h1>Notre histoire</h1>
+    <p>
+        Recevoir des fleurs fait toujours plaisir. Elles apportent de la joie, de la couleur et un instant d’émotion. Mais ce bonheur est souvent éphémère, car les fleurs finissent par faner.
+        C’est de ce constat qu’est née DK Bloom : l’envie de prolonger ces instants précieux en créant des fleurs qui durent pour l’éternité.
+        Nos créations allient l’élégance de la nature et la durabilité, pour que chaque bouquet devienne un souvenir intemporel, à offrir ou à s’offrir.
+    </p>
+
+    <div class="slider">
+        <div class="slide-track">
+            <img src="img/" alt="1"/>
+            <img src="img/" alt="2"/>
+            <img src="img" alt="3"/>
+            <img src="img" alt="4"/>
+            <img src="img" alt="5"/>
+
+            <!-- Répète les images pour boucle continue -->
+            <img src="img/" alt="1"/>
+            <img src="img/" alt="2"/>
+            <img src="img" alt="3"/>
+            <img src="img" alt="4"/>
+            <img src="img" alt="5"/>
+
+        </div>
+    </div>
+</main>
+<?php include 'includes/footer.php'; ?>
+</body>
