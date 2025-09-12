f5462eac1cc7646f88a16dd4fefad415f68ab77e
OSSENI YASMINATH MEGANE <YASMINATH.OSSN@ge-pedago.etat-ge.ch>
2025-09-12 09:54:53 +0200

    page a propos

--- site/pages/apropos.php
+++ site/pages/apropos.php
@@ -1,1 +1,13 @@
-<?php
+
+
+<?php include 'includes/header.php'; ?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>DK Bloom — Catalogue bouquet</title>
+    <link rel="stylesheet" href="css/style_connexion_inscription.css">
+</head>
