db806bd1f088966702527f1c282f47fc4003d77d
Astweed <johany.bstmn@eduge.ch>
2025-09-15 16:23:47 +0200

    Corrections fleur css

--- site/pages/fleur.php
+++ site/pages/fleur.php
@@ -110,35 +110,35 @@ if (!isset($BASE)) {
                 <!-- Radios (portent l'ID produit + nom + image) -->
                 <input type="radio" id="c-rouge"  name="rose-color" class="color-radio"
                        data-pro-id="1" data-name="Rose rouge"  data-img="<?= $BASE ?>img/rouge.png" checked>
-                <input type="radio" id="c-rose"   name="rose-color" class="color-radio"
-                       data-pro-id="2" data-name="Rose rose"   data-img="<?= $BASE ?>img/rose.png">
-                <input type="radio" id="c-roseC"  name="rose-color" class="color-radio"
-                       data-pro-id="3" data-name="Rose claire" data-img="<?= $BASE ?>img/rose_claire.png">
+                <input type="radio" id="c-roseC"   name="rose-color" class="color-radio"
+                       data-pro-id="2" data-name="Rose Claire"   data-img="<?= $BASE ?>img/rose_claire.png">
+                <input type="radio" id="c-rose"  name="rose-color" class="color-radio"
+                       data-pro-id="3" data-name="Rose rose" data-img="<?= $BASE ?>img/rose.png">
                 <input type="radio" id="c-blanc"  name="rose-color" class="color-radio"
                        data-pro-id="4" data-name="Rose blanche" data-img="<?= $BASE ?>img/rosesBlanche.png">
-                <input type="radio" id="c-noir"   name="rose-color" class="color-radio"
-                       data-pro-id="5" data-name="Rose noire"   data-img="<?= $BASE ?>img/noir.png">
                 <input type="radio" id="c-bleu"   name="rose-color" class="color-radio"
-                       data-pro-id="6" data-name="Rose bleue"   data-img="<?= $BASE ?>img/bleu.png">
+                       data-pro-id="5" data-name="Rose bleue"   data-img="<?= $BASE ?>img/bleu.png">
+                <input type="radio" id="c-noir"   name="rose-color" class="color-radio"
+                       data-pro-id="6" data-name="Rose noire"   data-img="<?= $BASE ?>img/noir.png">
 
                 <!-- Zone image -->
                 <div class="rose">
-                    <img src="<?= $BASE ?>img/rouge.png"        class="img-rose rouge"   alt="Rose rouge"   width="500">
-                    <img src="<?= $BASE ?>img/rose.png"         class="img-rose rose"    alt="Rose"         width="500">
-                    <img src="<?= $BASE ?>img/rose_claire.png"  class="img-rose roseC"   alt="Rose claire"  width="500">
-                    <img src="<?= $BASE ?>img/rosesBlanche.png" class="img-rose blanche" alt="Rose blanche" width="500">
-                    <img src="<?= $BASE ?>img/noir.png"         class="img-rose noire"   alt="Rose noire"   width="500">
-                    <img src="<?= $BASE ?>img/bleu.png"         class="img-rose bleue"   alt="Rose bleue"   width="500">
+                    <img src="<?= $BASE ?>img/rouge.png"        class="img-rose rouge"      alt="Rose rouge"    width="500">
+                    <img src="<?= $BASE ?>img/rose_claire.png"  class="img-rose roseC"      alt="Rose claire"   width="500">
+                    <img src="<?= $BASE ?>img/rose.png"         class="img-rose rose"       alt="Rose rose"     width="500">
+                    <img src="<?= $BASE ?>img/rosesBlanche.png" class="img-rose blanche"    alt="Rose blanche"  width="500">
+                    <img src="<?= $BASE ?>img/bleu.png"         class="img-rose bleue"      alt="Rose bleue"    width="500">
+                    <img src="<?= $BASE ?>img/noir.png"         class="img-rose noire"      alt="Rose noire"    width="500">
                 </div>
 
                 <!-- Pastilles (labels) -->
                 <fieldset class="swatches" aria-label="Couleur de la rose">
                     <label class="swatch" for="c-rouge"  title="Rouge"><span style="--swatch:red"></span></label>
-                    <label class="swatch" for="c-rose"   title="Rose"><span style="--swatch:#ffa0c4"></span></label>
-                    <label class="swatch" for="c-roseC"  title="Rose claire"><span style="--swatch:pink"></span></label>
+                    <label class="swatch" for="c-roseC"   title="Rose"><span style="--swatch:#ffa0c4"></span></label>
+                    <label class="swatch" for="c-rose"  title="Rose claire"><span style="--swatch:pink"></span></label>
                     <label class="swatch" for="c-blanc"  title="Blanc"><span style="--swatch:#e9e9e9"></span></label>
-                    <label class="swatch" for="c-noir"   title="Noir"><span style="--swatch:#111"></span></label>
-                    <label class="swatch" for="c-bleu"   title="Bleu"><span style="--swatch:#0418a5"></span></label>
+                    <label class="swatch" for="c-bleu"   title="Noir"><span style="--swatch:#0418a5"></span></label>
+                    <label class="swatch" for="c-noir"   title="Bleu"><span style="--swatch:#111"></span></label>
                 </fieldset>
 
                 <input type="number" class="qty" name="qty" min="1" max="99" step="1" value="1" inputmode="numeric">
