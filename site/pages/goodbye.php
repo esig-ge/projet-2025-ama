32afad6ba7e4f7cda12471adfe28730911242884
KURTESHI ARDITA <ARDITA.KRTSH@ge-pedago.etat-ge.ch>
2025-10-08 13:52:20 +0200

    maj

--- site/pages/goodbye.php
+++ site/pages/goodbye.php
@@ -1,1 +1,20 @@
+<?php
+
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>DK Bloom</title>
+
+
+</head>
+<body>
 <p>dommage</p>
+</body>
+</html>
